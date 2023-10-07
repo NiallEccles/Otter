a81b3d3519af1ac4a8c86455edd02271816b4bc9
EcclesN <Niall.Eccles@hmhco.com>
2023-10-07 11:33:47 +0100

    Add Switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './App.css'
+import { Switch } from './stories/Switch'
+
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       <div>
         <h1 className='bg-red-400'>Hello there!</h1>
       </div>
+      <Switch label='fre'/>
     </>
   )
 }
